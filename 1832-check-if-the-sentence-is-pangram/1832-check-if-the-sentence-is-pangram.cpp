cb96dfddb60e0fbbabfa97d32121f162e5218b91
Saim Ch <saimch726@gmail.com>
2026-07-04 23:49:54 +0500

    Time: 0 ms (100%), Space: 9 MB (36.38%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        unordered_set<int> s;
+        for(int i = 0; i < sentence.length(); i++){
+            s.insert(sentence[i]);
+        }
+        if(s.size() == 26){
+            return true;
+        }
+        return false;
+    }
+};
